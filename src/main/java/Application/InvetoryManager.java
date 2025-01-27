b41ac4975ee745c987377e16ec13dc16624a146c
carlog1566 <cgprojects2508@gmail.com>
2025-01-26 22:39:57 -0800

    title

--- src/main/java/Application/InvetoryManager.java
+++ src/main/java/Application/InvetoryManager.java
@@ -5,22 +5,31 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class InvetoryApp extends Application {
+public class InvetoryManager extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
+        Scene scene = new Scene(root, Color.WHITE);
+        stage.setMaximized(true);
 
         Image icon = new Image("InvetoryIcon.png");
         stage.getIcons().add(icon);
-        stage.setTitle("InvetoryApp");
-        stage.setMaximized(true);
+        stage.setTitle("InvetoryManager");
 
+        Text text = new Text();
+        text.setText("Invetory Manager");
+        text.setX(20);
+        text.setY(75);
+        text.setFont(Font.font("Verdana", 70));
+        text.setStyle("-fx-font-weight: bold");
 
+        root.getChildren().add(text);
         stage.setScene(scene);
         stage.show();
     }
